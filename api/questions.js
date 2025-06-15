dea256aac0d3413a3ea48e004706399eceb8094f
Zackie <102979057+MasterZack69@users.noreply.github.com>
2025-06-15 13:12:21 +0530

    Update questions.js

--- api/questions.js
+++ api/questions.js
@@ -37,10 +37,6 @@ export default function handler(req, res) {
         const questions = JSON.parse(decrypted.toString());
 
         
-        if (!req.headers.authorization) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
         // Return decrypted questions
         res.status(200).json(questions);
 
